62efb50b4ae9c3a8ae2259ad6502574cbfae941c
Iryna Finenko <irynafinenko@gmail.com>
2025-08-02 22:29:04 +0300

    Linked List

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,8 +3,31 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private static class Node<T> {
+        T value;
+        Node<T> prev;
+        Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
     @Override
     public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (size == 0) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
@@ -17,31 +40,37 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public T get(int index) {
+
         return null;
     }
 
     @Override
     public T set(T value, int index) {
+
         return null;
     }
 
     @Override
     public T remove(int index) {
+
         return null;
     }
 
     @Override
     public boolean remove(T object) {
+
         return false;
     }
 
     @Override
     public int size() {
+
         return 0;
     }
 
     @Override
     public boolean isEmpty() {
+
         return false;
     }
 }
